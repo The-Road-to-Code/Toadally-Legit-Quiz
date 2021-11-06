9f04cba6aba9983a792b40a7047a186fe7b3810e
patrick hrabos <phrabos@gmail.com>
2021-11-06 15:27:25 -0700

    take out test component from App

--- src/components/Results/TwitterButton.jsx
+++ src/components/Results/TwitterButton.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const TwitterButton = () => {
+const TwitterButton = ({frog}) => {
   return (
-    <button>post to Twitter</button>
+    <a href={`https://twitter.com/intent/tweet?url=www.deployed.com&text=look%20at%20my%20cool%20quiz%20results%20${frog.twitterImage}`} target="_blank" rel="noreferrer">post to Twitter</a>
   )
+}
+
+TwitterButton.propTypes = {
+  frog: PropTypes.object
 }
 
 export default TwitterButton;
