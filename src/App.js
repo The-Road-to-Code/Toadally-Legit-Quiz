b8aa3e2771af78f4c291bfbd3cac8ef29bb77791
Paul Brubaker <pbrubak@gmail.com>
2021-11-06 17:19:03 -0700

    fill out ResultsContainer component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import Header from './components/common/UI/Header';
 import HomeContainer from './containers/HomeContainer';
 import Footer from './components/common/UI/Footer';
 import QuizContainer from './containers/QuizContainer';
+import { ResultsContainer } from './containers/ResultsContainer';
+
 function App() {
+    const stateArray = [0,2,3];
     return (
         <div className="App">
             <Router>
@@ -15,6 +18,9 @@ function App() {
                     </Route>
                     {/* <Route exact path="/about" component={AboutContainer} /> */}
                     <Route exact path="/quiz" component={QuizContainer} />
+                    <Route exact path="/results">
+                        <ResultsContainer resultsArr={stateArray}/>
+                    </Route>
                 </Switch>
                 <Footer />
             </Router>
--- src/utils/quiz-results-helpers.js
+++ src/utils/quiz-results-helpers.js
@@ -1,6 +1,4 @@
-const fakeResults = [0, 3, 1];
-
-function getToadals(resultsArr, questions) {
+export function getToadals(resultsArr, questions) {
     let toadValuesLength = questions[0].toadValues[0].length;
     let toadals = new Array(toadValuesLength).fill(0);
 
@@ -15,13 +13,8 @@ function getToadals(resultsArr, questions) {
     return toadals;
 }
 
-function getFrog(totalsArr, frogsArr) {
+export function getFrog(totalsArr, frogsArr) {
     const max = Math.max(...totalsArr);
     const frogIndex = totalsArr.indexOf(max);
     return frogsArr[frogIndex];
 }
-
-// should return 7
-
-const totalsArr = getToadals(fakeResults, questions);
-console.log(getFrog(totalsArr, frogs));
